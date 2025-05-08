fe2bfbf45c198cd6e3fc2f47cc67674a4633957b
Kiran R Aithal <126346131+Kiran0604@users.noreply.github.com>
2025-05-08 18:10:18 +0530

    Time: 282 ms (68.35%) | Memory: 196.4 MB (19.20%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
-    typedef vector<int> P; // {time, {row, col}}
-    
     int minTimeToReach(vector<vector<int>>& moveTime) {
         int n = moveTime.size(), m = moveTime[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        using P = tuple<int, int, int, int>; // time, moves, row, col
         priority_queue<P, vector<P>, greater<P>> pq;
+        vector<vector<vector<bool>>> vis(n, vector<vector<bool>>(m, vector<bool>(2, false)));
         
-        dist[0][0] = 0;
-        pq.push({0,0,0,0});
-        
-        int dr[4] = {-1, 1, 0, 0};
-        int dc[4] = {0, 0, -1, 1};
-        
+        pq.push({0, 0, 0, 0}); // start at (0,0) with 0 time and 0 moves
+        vis[0][0][0] = true;
+        int dy[4] = {-1, 1, 0, 0}, dx[4] = {0, 0, -1, 1};
+
         while (!pq.empty()) {
-            auto X = pq.top(); 
-            int curTime=X[0],r=X[1],c=X[2],flag=X[3];
-            pq.pop();    
-            if (r == n - 1 && c == m - 1)
-                return curTime;
-            
+            auto [time, moves, r, c] = pq.top(); pq.pop();
+
             for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i], nc = c + dc[i];
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && dist[nr][nc]==INT_MAX) {
-                    if(dist[r][c]<=moveTime[nr][nc])
-                    {
-                        if(flag==1)
-                            dist[nr][nc]=moveTime[nr][nc]+2;
-                        else 
-                            dist[nr][nc]=moveTime[nr][nc]+1;
-                    }
-                    else 
-                    {
-                        if(flag==1)
-                            dist[nr][nc]=min(dist[r][c]+2,dist[nr][nc]);
-                        else 
-                            dist[nr][nc]=min(dist[r][c]+1,dist[nr][nc]);
-                    }
-                    
-                    pq.push({dist[nr][nc],nr,nc,!flag});
-                }
+                int nr = r + dy[i], nc = c + dx[i];
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+                
+                int newMoves = moves + 1;
+                int parity = newMoves % 2;
+                if (vis[nr][nc][parity]) continue;
+                
+                int cost = (newMoves % 2 == 1) ? 1 : 2; // odd moves: 1, even moves: 2
+                int wait = max(0, moveTime[nr][nc] - time); // wait if arriving early
+                int newTime = time + wait + cost;
+                
+                if (nr == n-1 && nc == m-1) return newTime;
+                
+                vis[nr][nc][parity] = true;
+                pq.push({newTime, newMoves, nr, nc});
             }
         }
         return -1;
