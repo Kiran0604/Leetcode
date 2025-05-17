b4d1d7e1b3019f16ba2fb4f9687342ab93f35282
Kiran R Aithal <126346131+Kiran0604@users.noreply.github.com>
2025-05-17 21:31:39 +0530

    Time: 0 ms (100.00%) Space: 10.1 MB (89.99%)

--- 0198-house-robber/house-robber.cpp
+++ 0198-house-robber/house-robber.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return nums[0];
-
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; ++i) {
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        int prev1 = 0, prev2 = 0;
+        for (int num : nums) {
+            int curr = max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = curr;
         }
-
-        return dp[n - 1];
+        return prev1;
     }
 };
