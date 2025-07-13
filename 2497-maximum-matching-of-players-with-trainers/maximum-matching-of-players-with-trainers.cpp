5d23daf2ba46be3f27c7b47a202ed81f6eafda3d
Kiran R Aithal <126346131+Kiran0604@users.noreply.github.com>
2025-07-13 23:14:39 +0530

    Time: 29 ms (55.28%) Space: 80 MB (97.54%)

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-        sort(trainers.begin(), trainers.end());
-        sort(players.begin(), players.end());
-        int m = players.size(), n = trainers.size(), j = 0, c = 0, i = 0;
-        while (i < m && j < n) {
-            if (players[i] <= trainers[j]) {
-                c++;
-                i++;
-            }
+    int matchPlayersAndTrainers(vector<int>& p, vector<int>& t) {
+        sort(p.begin(), p.end());
+        sort(t.begin(), t.end());
+        int i = 0, j = 0;
+        while(i < p.size() && j < t.size()){
+            if(p[i] <= t[j]) i++;
             j++;
         }
-        return c;
+        return i;
     }
 };
