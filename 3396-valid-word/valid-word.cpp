73362fa588ff636ce26196ea16b795dbcea922c3
Kiran R Aithal <126346131+Kiran0604@users.noreply.github.com>
2025-07-15 11:42:13 +0530

    Time: 0 ms (100.00%) Space: 8.1 MB (33.01%)

---
+++ 3396-valid-word/valid-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        if (word.size() < 3) return false;
+
+        int vowels = 0, consonants = 0;
+
+        for (char ch : word) {
+            if (!isalnum(ch))
+                return false;
+
+            char lower = tolower(ch);
+            if (isalpha(lower)) {
+                if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u')
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+
+        return vowels > 0 && consonants > 0;
+    }
+};
